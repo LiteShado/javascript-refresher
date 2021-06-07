1417f909ed065332e7b0dea090a7625d9c3a3338
Natalie <theliteshado@icloud.com>
2021-06-07 07:34:36 -0400

    tests

---
+++ test.js
@@ -0,0 +1,38 @@
+/*
+Title" Area of Circle
+*/
+
+// function area(radius) {
+//     let area = 3.1416 * radius * radius;
+//     return area;
+// }
+
+// console.log(area(5))
+
+// function check(num) {
+//     if(num%2==0){
+//         return "Even";
+//     }
+//     if(num%2==1) {
+//         return "Odd"
+//     }
+// }
+
+// console.log(check(5))
+
+// let n1 = 1;
+// let n2 = 2;
+
+// let tmp = n1;
+// n1 = tmp;
+// console.log("n1", n1, "n2", n2);
+
+// let n1 = 1;
+// let n2 = 2;
+
+// n1 = n1 * n2;
+// n2 = n1 / n2;
+// n1= n1 / n2;
+// console.log("n1", n1, "n2", n2)
+
+
