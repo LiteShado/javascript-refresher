3530c9d30cef3edf649b8fe4b113ad68e3492e17
Natalie <theliteshado@icloud.com>
2021-06-08 21:28:12 -0400

    edits

--- test4.js
+++ test4.js
@@ -24,8 +24,41 @@
 
 //write a function that takes a strings and a character. if the strings end with the character then return true, else return false.
 
-function confirm_ending(str, ch) {
-    let reversed = str.split("").reverse();
-    return reversed[0] === ch;
-}
-console.log(confirm_ending('hello', 'o'));
+// function confirm_ending(str, ch) {
+//     let reversed = str.split("").reverse();
+//     return reversed[0] === ch;
+// }
+// console.log(confirm_ending('hello', 'o'));
+
+
+//write a function that takes range of a number "9-11", return the sum of number in between of the range.
+
+// function sumInRange(range) {
+//     let arr = range.split('-');
+//     console.log(arr);
+// }
+// sumInRange('1-4');
+
+// function sumInRange(range) {
+//     let arr = range.split("-");
+//     let result = 0;
+//     for(let i=arr[0]; i<arr[1]; i++) {
+//         result += parseInt(i);
+//     }
+//     return result;
+// }
+// console.log(sumInRange("10-20"));
+
+
+
+
+//Sorting Array
+// write a function that takes an array, return ascending of the array.
+
+// function asc(arr) {
+//     return arr.sort((a, b) => {
+//         return a - b;
+//     })
+// }
+// console.log(asc([1, 4, 2, 3]));
+
